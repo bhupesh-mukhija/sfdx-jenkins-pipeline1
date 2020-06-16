9e8233c957a63cb8efcc0930d0fd49134bdc717b
bhupesh-sage <bhupesh.mukhija@sage.com>
2020-06-16 18:00:20 +0100

    Remove unecessary files and add .gitignore

--- .sfdx/typings/lwc/staticresources.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "@salesforce/resourceUrl/GIPHY" {
-    var GIPHY: string;
-    export default GIPHY;
-}
-declare module "@salesforce/resourceUrl/jquery331" {
-    var jquery331: string;
-    export default jquery331;
-}
